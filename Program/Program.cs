257522767671aa4b95b5325978e87b276e329fee
DYK42 <yurandk42@gmail.com>
2022-09-15 10:47:49 +0700

    add method ArrayFilling()

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+string[] ArrayFilling()
+{
+    Console.WriteLine("Вводите для заполнения массива строки через Enter. Для прекращения введите пустую строку.");
+    string result = string.Empty;
+    int count = 0;
+
+    while (true)
+    {
+        string txt = Console.ReadLine();
+
+        if(txt == string.Empty) break;
+        result += (count <=0 ? "" : ",") + txt;
+        count++;
+    }
+
+    string[] array = result.Split(",");
+
+    return array;
+}
